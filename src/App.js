3d9fac42cdf001b2d473b969e82ec3afc4868a19
Lees <lishan@helputalk.com>
2020-10-28 14:08:04 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { createStore } from 'redux'
+function App () {
   return (
     <div className="App">
       <header className="App-header">
